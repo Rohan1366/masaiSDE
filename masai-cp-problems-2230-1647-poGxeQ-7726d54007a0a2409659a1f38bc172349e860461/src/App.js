80bf95ce9f18bb48f0d43e754491eeb3cffae23b
Rohan Ghadge <113715810+Rohan1366@users.noreply.github.com>
2023-09-29 19:49:41 +0530

    added

--- masai-cp-problems-2230-1647-poGxeQ-7726d54007a0a2409659a1f38bc172349e860461/src/App.js
+++ masai-cp-problems-2230-1647-poGxeQ-7726d54007a0a2409659a1f38bc172349e860461/src/App.js
@@ -3,6 +3,7 @@ const App = () => {
   return (
     <div className="container">
          {/* Add title here in h1 tag */}
+         <h1></h1>
       <div>
         {/* Add BillPage component */}
       </div>
